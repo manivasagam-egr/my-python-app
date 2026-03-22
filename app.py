917a38b54e57775fb7606717e64d7d7ff28553d5
manivasagam-egr <manivasagamegr@gmail.com>
2026-03-22 21:01:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
+import os
 
 class handler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
         self.end_headers()
-        self.wfile.write(b"Hello DevOps Manivasagam vaazhthukkal!")
+        self.wfile.write("Hello DevOps 🚀 Manivasagam!".encode())
 
-server = HTTPServer(('0.0.0.0', 3000), handler)
-print("Server running on port 3000...")
+port = int(os.environ.get("PORT", 8080))
+
+server = HTTPServer(('0.0.0.0', port), handler)
+print(f"Server running on port {port}...")
 server.serve_forever()
