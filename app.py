d58549621631a5ee3768037ddf49633ee401db83
manivasagam-egr <manivasagamegr@gmail.com>
2026-03-22 21:09:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ class handler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
         self.end_headers()
-        self.wfile.write("Hello DevOps 🚀 Manivasagam!".encode())
+        self.wfile.write("Hello DevOps super deployment Manivasagam!".encode())
 
 port = int(os.environ.get("PORT", 8080))
 
